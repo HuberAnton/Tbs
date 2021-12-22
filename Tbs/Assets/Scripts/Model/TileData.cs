cb33c385aa7f182c94dc26022d99d7403da698fe
Anton <huber.anton@hotmail.com>
2021-12-22 17:11:58 +1100

    Tile Data
    - Creating a new model for tile storage.
    - Need to add a "feature check" event on moving over tiles. With pauses and continues.

---
+++ Tbs/Assets/Scripts/Model/TileData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// All data needed for creation of a level tile.
+public class TileData
+{
+    Vector3 Position;
+    // Being from water, rock grass ect.
+    int TileSet;
+    // Ramp, cliff, empty space, wall pattern. 
+    int TileType;
+    // Scripts to be added to the tile.
+    // Eg Spawn point, Reinforcment point, deployment point.
+    List<GameObject> Features;
+
+    // If a unit start off here.
+    GameObject Content;
+}
